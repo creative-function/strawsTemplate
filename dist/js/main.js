7bf9e9624d2e4522971b1094f2a41aee21131f7b
Chelsea <chelsea.campbell@my.creativecirucs.edu>
2019-07-30 10:42:15 -0400

    straw template

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,4 +1,10 @@
 "use strict";
 
 console.log("Hello World from main.js! \nChange this message, and make sure it changes in the browser \nto verify that you're working in the right files.");
+var photoBox = document.querySelectorAll(".single-project");
+var photoGallery = ['url("/dist/img/dog1.jpg")', 'url("/dist/img/dog2.jpg")', 'url("/dist/img/dog3.jpg")', 'url("/dist/img/dog4.jpg")', 'url("/dist/img/dog5.jpg")', 'url("/dist/img/dog6.jpg")', 'url("/dist/img/dog7.jpg")', 'url("/dist/img/dog8.jpg")', 'url("/dist/img/dog9.jpg")', 'url("/dist/img/dog10.jpg")', 'url("/dist/img/dog11.jpg")', 'url("/dist/img/dog12.jpg")'];
+$('.single-project').css({
+  "background-image": photoGallery[0],
+  "background-size": "contain"
+});
 //# sourceMappingURL=main.js.map
--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,26 @@
 console.log(`Hello World from main.js! 
 Change this message, and make sure it changes in the browser 
 to verify that you're working in the right files.`)
+
+
+let photoBox = document.querySelectorAll(".single-project");
+
+let photoGallery = [
+   'url("/dist/img/dog1.jpg")',
+   'url("/dist/img/dog2.jpg")',
+   'url("/dist/img/dog3.jpg")',
+   'url("/dist/img/dog4.jpg")',
+   'url("/dist/img/dog5.jpg")',
+   'url("/dist/img/dog6.jpg")',
+   'url("/dist/img/dog7.jpg")',
+   'url("/dist/img/dog8.jpg")',
+   'url("/dist/img/dog9.jpg")',
+   'url("/dist/img/dog10.jpg")',
+   'url("/dist/img/dog11.jpg")',
+   'url("/dist/img/dog12.jpg")'
+]
+
+$('.single-project').css({
+    "background-image" : photoGallery[0],
+    "background-size" : "contain"
+});
